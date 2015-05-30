7a4d4311a27c3d9580a69abfcc55b8b3d63712b5
zukutrule <sader.ale@gmail.com>
2015-05-30 00:50:09 -0300

    initial commit

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,15 @@
+//GET /quizes/question
+exports.question = function (reg, res){
+	res.render('quizes/question', {pregunta: 'Capital de italia', title: 'Quiz' });
+
+};
+
+//GET /quizes/answer
+exports.answer = function (req, res){
+	if(req.query.respuesta === 'Roma'){
+		res.render('quizes/answer', {respuesta: 'Correcto', title: 'Quiz' });
+	}else{
+		res.render('quizes/answer', {respuesta: 'Incorrecto', title: 'Quiz' });
+	}
+
+};

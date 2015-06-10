c0543fee5a1af8257f033d2f5d0a99fb6a0f81c1
zukutrule <sader.ale@gmail.com>
2015-06-10 01:43:11 -0300

    despliege DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,21 @@
-//GET /quizes/question
-exports.question = function (reg, res){
-	res.render('quizes/question', {pregunta: 'Capital de italia', title: 'Quiz' });
-
+var models = require('../models/models.js');
+// GET /quizes/question
+exports.question = function(req, res) {
+  models.Quiz.findAll().then(function(quiz) {
+  	console.log("checa la tablaaaaaaaaaaaaaa");
+    res.render('quizes/question', { pregunta: quiz[0].pregunta, title: 'pregunta' });
+  })
 };
 
-//GET /quizes/answer
-exports.answer = function (req, res){
-	if(req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto', title: 'Quiz' });
-	}else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto', title: 'Quiz' });
-	}
+// GET /quizes/answer
+exports.answer = function(req, res) {
+  models.Quiz.findAll().then(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto', title: 'respuesta' });
+    } else {
+      res.render('quizes/answer', { respuesta: 'Incorrecto', title: 'respuesta' });
+    }
+  })
 };
 //GET /author
 exports.author = function (req, res){
